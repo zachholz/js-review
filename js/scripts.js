edd4381bd23d7c6a00ea6f8a7a237e81de355335
Zach Holz <zach.a.holz@gmail.com>
2016-06-11 19:39:07 -0700

    Complete js logic with all options and hide/show methods

--- js/scripts.js
+++ js/scripts.js
@@ -10,14 +10,125 @@ $(document).ready(function() {
   	// Vacation picker results
     if(quizQuestion1 === "abroad" && quizQuestion2 === "urban" && quizQuestion3 === "mountain" && quizQuestion4 === "modern" && quizQuestion5 === "adventure") {
     	$("#santiago").show();
+    	$("#takayama").hide();
+    	$("#tokyo").hide();
+    	$("#amsterdam").hide();
+    	$("#rio").hide();
+    	$("#iceland").hide();
+    	$("#thailand").hide();
+    	$("#domestic").hide();
+    	$("#none").hide();
+    	$("#munich").hide();
     } else if(quizQuestion1 === "abroad" && quizQuestion2 === "urban" && quizQuestion3 === "mountain" && quizQuestion4 === "history" && quizQuestion5 === "relax") {
     	$("#munich").show();
+    	$("#santiago").hide();
+    	$("#takayama").hide();
+    	$("#tokyo").hide();
+    	$("#amsterdam").hide();
+    	$("#rio").hide();
+    	$("#iceland").hide();
+    	$("#thailand").hide();
+    	$("#domestic").hide();
+    	$("#none").hide();
     } else if(quizQuestion1 === "abroad" && quizQuestion2 === "urban" && quizQuestion3 === "mountain" && quizQuestion4 === "history" && quizQuestion5 === "adventure") {
     	$("#takayama").show();
+    	$("#munich").hide();
+    	$("#santiago").hide();
+    	$("#tokyo").hide();
+    	$("#amsterdam").hide();
+    	$("#rio").hide();
+    	$("#iceland").hide();
+    	$("#thailand").hide();
+    	$("#domestic").hide();
+    	$("#none").hide();
     } else if(quizQuestion1 === "abroad" && quizQuestion2 === "urban" && quizQuestion3 === "mountain" && quizQuestion4 === "modern" && quizQuestion5 === "relax") {
-    	$("#tokyo").show();			
+    	$("#tokyo").show();	
+    	$("#takayama").hide();
+    	$("#munich").hide();
+    	$("#santiago").hide();
+    	$("#amsterdam").hide();
+    	$("#rio").hide();
+    	$("#iceland").hide();
+    	$("#thailand").hide();
+    	$("#domestic").hide();
+    	$("#none").hide();			
     } else if(quizQuestion1 === "abroad" && quizQuestion2 === "urban" && quizQuestion3 === "beach" && quizQuestion4 === "history" && quizQuestion5 === "relax") {
-    	$("#amsterdam").show(); 
+    	$("#amsterdam").show();
+    	$("#tokyo").hide();	
+    	$("#takayama").hide();
+    	$("#munich").hide();
+    	$("#santiago").hide();
+    	$("#rio").hide();
+    	$("#iceland").hide();
+    	$("#thailand").hide();
+    	$("#domestic").hide();
+    	$("#none").hide(); 
+    } else if(quizQuestion1 === "abroad" && quizQuestion2 === "urban" && quizQuestion3 === "beach") {
+    	$("#rio").show();	
+    	$("#amsterdam").hide();
+    	$("#tokyo").hide();	
+    	$("#takayama").hide();
+    	$("#munich").hide();
+    	$("#santiago").hide();
+    	$("#iceland").hide();
+    	$("#thailand").hide();
+    	$("#domestic").hide();
+    	$("#none").hide(); 
+		} else if(quizQuestion1 === "abroad" && quizQuestion2 === "urban" && quizQuestion3 === "beach" && quizQuestion4 === "modern" && quizQuestion5 === "adventure") {
+			$("#tokyo").show();
+			$("#takayama").hide();
+    	$("#munich").hide();
+    	$("#santiago").hide();
+    	$("#amsterdam").hide();
+    	$("#rio").hide();
+    	$("#iceland").hide();
+    	$("#thailand").hide();
+    	$("#domestic").hide();
+    	$("#none").hide(); 	
+	  } else if(quizQuestion1 === "abroad" && quizQuestion2 === "countryside" && quizQuestion3 === "beach" && quizQuestion4 === "modern" && quizQuestion5 === "relax") {
+			$("#iceland").show();
+			$("#rio").hide();
+			$("#amsterdam").hide();
+    	$("#tokyo").hide();	
+    	$("#takayama").hide();
+    	$("#munich").hide();
+    	$("#santiago").hide();
+    	$("#thailand").hide();
+    	$("#domestic").hide();
+    	$("#none").hide();			
+		} else if(quizQuestion1 === "abroad" && quizQuestion2 === "countryside" && quizQuestion3 === "mountain") {
+			$("#munich").show();
+			$("#santiago").hide();
+    	$("#takayama").hide();
+    	$("#tokyo").hide();
+    	$("#amsterdam").hide();
+    	$("#rio").hide();
+    	$("#iceland").hide();
+    	$("#thailand").hide();
+    	$("#domestic").hide();
+    	$("#none").hide();
+		} else if(quizQuestion1 === "abroad" && quizQuestion2 === "countryside" && quizQuestion3 === "beach") {
+			$("#thailand").show();
+			$("#munich").hide();
+			$("#santiago").hide();
+    	$("#takayama").hide();
+    	$("#tokyo").hide();
+    	$("#amsterdam").hide();
+    	$("#rio").hide();
+    	$("#iceland").hide();
+    	$("#domestic").hide();
+    	$("#none").hide();																
+		} else if(quizQuestion1 === "domestic") {
+			$("#domestic").show();
+			$("#thailand").hide();
+			$("#munich").hide();
+			$("#santiago").hide();
+    	$("#takayama").hide();
+    	$("#tokyo").hide();
+    	$("#amsterdam").hide();
+    	$("#rio").hide();
+    	$("#iceland").hide();
+    	$("#none").hide();								
   	} else {
   		$("#none").show();
   	}
